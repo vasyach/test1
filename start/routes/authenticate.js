6e0b2dc9941a22965a4faea66d749a1f07ba5015
Vasiliy Chaniev <vasiliy@Vasiliys-MacBook-Pro.local>
2016-04-24 02:58:43 +0300

    some fixes 7

--- start/routes/authenticate.js
+++ start/routes/authenticate.js
@@ -5,16 +5,23 @@ module.exports = function(passport){
 
     //sends successful login state back to angular
     router.get('/success', function(req, res){
-        console.log('req.user= is HEREEE');
-        //res.render('index', { title: 'Express' });
-        //res.send({state: 'success'});
+        console.log('req.user= is HEREEE ' + req.user);
+        res.send({state: 'success', user: req.user ? req.user : null});
+    });
+
+    router.post('/success', function(req, res){
+        console.log('req.user= is HEREEE ' + req.user);
         res.send({state: 'success', user: req.user ? req.user : null});
         
-        //res.redirect('/');
     });
+
 
     //sends failure login state back to angular
     router.get('/failure', function(req, res){
+        res.send({state: 'failure', user: null, message: "Invalid username or password"});
+    });
+
+    router.post('/failure', function(req, res){
         res.send({state: 'failure', user: null, message: "Invalid username or password"});
     });
 
