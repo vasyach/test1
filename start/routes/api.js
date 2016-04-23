22b1efe329d7eca18abc4d3db616a9115f63163c
Vasiliy Chaniev <vasiliy@Vasiliys-MacBook-Pro.local>
2016-04-23 20:02:35 +0300

    api route posts/id

--- start/routes/api.js
+++ start/routes/api.js
@@ -3,14 +3,29 @@ var router = express.Router();
 
 router.route('/posts')
 	.get(function(req, res){
-		res.send({messgae: 'todo Return all post'});
+		res.send({messgae: 'TODO Return all post'});
 
 	})
 	.post(function(req,res) {
 		// body...
-		res.send({message: 'todo Create a new post'});
+		res.send({message: 'TODO Create a new post'});
 	});
-	 
+
+router.route('/posts/:id')
+
+	//returns a particullar post
+	.get(function(req,res){
+		res.send({message: 'TODO Return post with ID ' + req.params.id});
+	})
+
+	// update existing post 
+	.put(function(req,res){
+		res.send({message: 'TODO modify post with ID ' + req.params.id}); 
+	})
+
+	// delete existing post 
+	.delete(function(req,res){
+		res.send({message: 'TODO delete a post with ID ' + req.params.id}); 
+	});
 
 module.exports = router;
-
