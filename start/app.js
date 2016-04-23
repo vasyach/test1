9d18815c67c37ce9e1f813e1bc4427eb3ab33516
Vasiliy Chaniev <vasiliy@Vasiliys-MacBook-Pro.local>
2016-04-23 19:23:56 +0300

    api route 4

--- start/app.js
+++ start/app.js
@@ -30,7 +30,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 //
-app.use('./api',api);
+app.use('/api',api);
 
 //app/use(passport.initialize());
 //app.use(passport.session());
